714da9eef37702fbcaadea851ebbeefbeef25b89
Scott Robbins <srscottrobbins@gmail.com>
2015-09-02 00:51:32 -0400

    .gitignore is now working

--- Example/Pods/Target Support Files/Pods-RxCocoaTouch_Example/Pods-RxCocoaTouch_Example-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_RxCocoaTouch_ExampleVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_RxCocoaTouch_ExampleVersionString[];
-
--- Example/Pods/Target Support Files/RxCocoaTouch/RxCocoaTouch-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_RxCocoaTouch : NSObject
-@end
-@implementation PodsDummy_RxCocoaTouch
-@end
